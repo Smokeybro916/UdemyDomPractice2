0cfd565bc058bf49a886a94165066b477d3775ac
Smokeybro916 <kyleluangasa@gmail.com>
2023-09-21 14:27:02 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+
+document.querySelectorAll(".done")
+const doneTodos = document.querySelectorAll(".done");
+document.querySelector("input[type='checkbox']")
+const checkbox = document.querySelector("input[type='checkbox']");
+
+/* Using document.querySelector */
